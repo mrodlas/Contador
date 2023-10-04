5a8188f2edf16dfb7fcd65ce04c27f564f449505
mrodlas <mariarodriguezlastres@gmail.com>
2023-10-04 09:34:50 +0200

    Proyecto Contador

---
+++ app/src/main/java/com/mgh/pmdm/contador/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.mgh.pmdm.contador
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.mgh.pmdm.contador.R.id.botonMenos
+
+class MainActivity : AppCompatActivity() {
+
+    var contador=0
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Referencia al TextView
+        val textViewContador=findViewById<TextView>(R.id.textViewContador)
+
+        // Inicializamos el TextView con el contador a 0
+        textViewContador.setText(contador.toString())
+
+        // Referencia al botón
+        val btAdd=findViewById<Button>(R.id.btAdd)
+        val botonMenos=findViewById<Button>(botonMenos)
+
+        // Asociaciamos una expresióin lambda como
+        // respuesta (callback) al evento Clic sobre
+        // el botón
+        btAdd.setOnClickListener {
+            contador++
+            textViewContador.setText(contador.toString())
+        }
+
+        botonMenos.setOnClickListener{
+            contador--
+            textViewContador.setText(contador.toString())
+        }
+
+    }
+}
